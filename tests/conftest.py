cde07cddd109411b741c378dc1a8f1ff542a896e
philippe <pythonjsdev@gmail.com>
2022-12-16 10:56:19 +0100

    ajout des tests

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+@pytest.fixture
+def source_path_root():
+    return r'D:\users\test backup manager\vanilla'
+
+
+@pytest.fixture
+def target_path_root():
+    return r'E:\DEV 2022 novembre 06\DEV\apps_js\vanilla'
